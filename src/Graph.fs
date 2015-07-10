c50159b44eb3377d0dd3db85923df32fdb6b2d00
Chang Lan <clan@eecs.berkeley.edu>
2015-07-09 18:09:15 -0700

    Add vertex and edge weight

--- src/Graph.fs
+++ src/Graph.fs
@@ -7,12 +7,14 @@ open QuickGraph.Serialization
 type VF = {
     name: string;
     t: string;
+    weight: double;
 }
 
 type Attribute = {
     filter: string;
     attr: string;
     pipelet_id: int;
+    weight: double;
 }
 
 type FileDotEngine() = 
@@ -26,15 +28,16 @@ type Graph(state: Parser.ParseState) =
     
     // Add vertices
     let _ = 
-        let vfs = state.V |> Map.toList |> List.map (fun (key, value) -> {name = key; t = value})
+        let vfs = state.V |> Map.toList |> List.map (fun (key, value) -> 
+            {name = key; t = value; weight = 1.0})
         (vfs |> List.fold (fun result vf -> result && graph.AddVertex(vf)) true)
     
     // Add edges
     let _ = (state.E |> List.mapi (fun i lst ->
         let add_edge result (v1, v2, e1, e2) =
-            let tag = {filter = e1; attr = e2; pipelet_id = i}
-            let vertex1 = {name = v1; t = state.V.[v1]}
-            let vertex2 = {name = v2; t = state.V.[v2]}
+            let tag = {filter = e1; attr = e2; pipelet_id = i; weight = 0.0}
+            let vertex1 = {name = v1; t = state.V.[v1]; weight = 1.0}
+            let vertex2 = {name = v2; t = state.V.[v2]; weight = 1.0}
             let edge = new TaggedEdge<VF, Attribute>(vertex1, vertex2, tag)
             result && graph.AddEdge(edge)
         List.fold add_edge true lst) |> List.fold (fun result r -> result && r) true)
