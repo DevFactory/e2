41d8bf520644b114f4a7ce8864ebb4ee89e6bbb0
Aurojit Panda <apanda@cs.berkeley.edu>
2015-11-24 11:44:25 -0800

    Deserialization

--- bessctl/BessSer.fs
+++ bessctl/BessSer.fs
@@ -1,6 +1,7 @@
 module BessSerialization
 open System.Collections.Generic
 open System
+open System.Reflection
 
 (* TODO: This only works for little endian right now, add support for running on
  * big endian machines *)
@@ -10,41 +11,30 @@ type BessMessage () =
   inherit Attribute()
 end
 
+(* Frame messages before sending them to BESS by adding length *)
 let private addLength (data: byte array) =
   Array.concat [|(System.BitConverter.GetBytes (int data.Length)); 
                  data |]
 
+(* Add BESS CTL channel messge *)
 let private encode (t: int) (len: int) (data : byte array) =
   Array.concat [|(System.BitConverter.GetBytes t);
                  (System.BitConverter.GetBytes len);
                  data|]
 
-let private pad8Bytes (a: byte array) =
-  let l = a.Length + 1
+(* BESS strings need to be 8 byte padded, figure out how many bytes to add *)
+let private find8BytePadding (length: int) =
+  let l = length + 1
   let m = l % 8
   let p = (8 - m) + 1
-  if m = 0 then Array.append a (Array.zeroCreate 1) 
-  else Array.append a (Array.zeroCreate p)
+  if m = 0 then 1 else p
 
-(* Encode NIL type. This should really just be a constant *)
-let private encodeNone () =
-  encode 0l 0l [||]
+(* Pad by 8 bytes *)
+let private pad8Bytes (a: byte array) =
+  let p = find8BytePadding(a.Length)
+  Array.append a (Array.zeroCreate p)
 
-let private encodeInt (a: int64) =
-  encode 1l 8l (System.BitConverter.GetBytes a)
-  
-let private encodeDouble (a: double) =
-  encode 2l 8l (System.BitConverter.GetBytes a)
-
-let private encodeString (a: string) =
-  (System.Text.Encoding.ASCII.GetBytes a 
-  |> pad8Bytes 
-  |> encode 3l a.Length)
-  
-let private encodeBlob (a: byte array) =
-  pad8Bytes a
-  |> encode 4l a.Length
-
+(* Some active patterns *)
 let (|IsList|_|) (candidate: obj) =
   let t = candidate.GetType()
   if t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<list<_>>
@@ -61,7 +51,56 @@ let (|IsBessMessage|_|) (candidate: obj) =
   let t = candidate.GetType()
   match t.GetCustomAttributes(typedefof<BessMessage>, true) with
   | [||] -> None
-  | _ -> Some(obj)
+  | _ -> Some(candidate)
+
+(* Constants for encoding *)
+[<Literal>]
+let NilType = 0l
+[<Literal>]
+let NilLength = 0l
+
+[<Literal>]
+let IntType = 1l
+[<Literal>]
+let IntLength = 8l
+
+[<Literal>]
+let DoubleType = 2l
+[<Literal>]
+let DoubleLength = 8l
+
+[<Literal>]
+let StringType = 3l
+
+[<Literal>]
+let BlobType = 4l
+
+[<Literal>]
+let ListType = 5l
+
+[<Literal>]
+let MapType = 6l
+
+(* Encode NIL type. This should really just be a constant *)
+let private encodeNone () =
+  encode NilType NilLength [||]
+
+(* Encode integer *)
+let private encodeInt (a: int64) =
+  encode IntType IntLength (System.BitConverter.GetBytes a)
+  
+(* Encode double *)
+let private encodeDouble (a: double) =
+  encode DoubleType DoubleLength (System.BitConverter.GetBytes a)
+
+let private encodeString (a: string) =
+  (System.Text.Encoding.ASCII.GetBytes a 
+  |> pad8Bytes 
+  |> encode StringType a.Length)
+  
+let private encodeBlob (a: byte array) =
+  pad8Bytes a
+  |> encode BlobType a.Length
 
 let rec private encodeObj enc =
   match box enc with
@@ -79,7 +118,7 @@ and private encodeList (a: System.Collections.IEnumerable) =
   asSeq 
   |> Seq.map encodeObj
   |> Array.concat
-  |> encode 5 (Seq.length asSeq)
+  |> encode ListType (Seq.length asSeq)
 and private strBytes (s:string) = 
   System.Text.Encoding.ASCII.GetBytes s
   |> pad8Bytes
@@ -90,7 +129,7 @@ and private encodeMap (a: System.Collections.IDictionary) =
   Seq.zip kbytes vobjs
   |> Seq.collect (fun x -> [(fst x); (snd x)])
   |> Array.concat
-  |> encode 6 (Seq.length kseq)
+  |> encode MapType (Seq.length kseq)
 and private encodeObject (o: obj) =
   let properties = o.GetType().GetProperties() |> 
                    Array.filter (fun x -> x.CanRead)
@@ -100,8 +139,116 @@ and private encodeObject (o: obj) =
     [(strBytes prop.Name); (encodeObj value)])
   |> Seq.collect (fun x -> x)
   |> Array.concat
-  |> encode 6 (Seq.length properties)
+  |> encode MapType (Seq.length properties)
 
 let Encode encode =
   encodeObj encode 
   |> addLength
+
+(* Decoding *)
+let private getTypeAndLength (bstream: byte array) =
+  let t = System.BitConverter.ToInt32(bstream, 0)
+  let l = System.BitConverter.ToInt32(bstream, 4)
+  (t, l)
+  
+let DecodeInt (bstream: byte array) =
+  let (t, l) = getTypeAndLength bstream
+  if t = IntType && l = IntLength then System.BitConverter.ToInt64(bstream, 8)
+  else failwith "Bad type and length, not an integer"
+
+let DecodeDouble (bstream: byte array) =
+  let (t, l) = getTypeAndLength bstream
+  if t = DoubleType && l = DoubleLength then System.BitConverter.ToDouble(bstream, 8)
+  else failwith "Bad type and length, not a double"
+
+let private decodeStringLen (bstream: byte array) =
+  let (t, l) = getTypeAndLength bstream
+  if t = StringType then
+    let s = System.Text.Encoding.ASCII.GetString(bstream, 8, l)
+    let p = find8BytePadding l
+    (8 + l + p, (box s))
+  else failwith "Bad type and length, not an ASCII string"
+
+let DecodeString (bstream: byte array) =
+  let (_, b) = (decodeStringLen bstream)
+  unbox b
+
+let private decodeBlobLen (bstream: byte array) =
+  let (t, l) = getTypeAndLength bstream
+  if t = BlobType then (8 + l, (box bstream.[8..(8+l)]))
+  else failwith "Not a byte stream"
+
+let DecodeBlob (bstream: byte array) =
+  let (_, b) = (decodeBlobLen bstream)
+  unbox b
+
+let rec private decodeWithLength (bstream: byte array) =
+  let t = System.BitConverter.ToInt32(bstream, 0)
+  match t with 
+  | 0 -> (8, null)
+  | 1 -> (16, box (DecodeInt bstream)) // boxing since allow normal types too :(
+  | 2 -> (16, box (DecodeDouble bstream))
+  | 3 -> decodeStringLen bstream
+  | 4 -> decodeBlobLen bstream
+  | 5 -> decodeListLen bstream
+  | 6 -> decodeMapLen bstream
+  | _ -> failwith (sprintf "Unsupported type %d" t)
+and private decodeListLen (bstream: byte array) =
+  let (t, l) = getTypeAndLength bstream
+  if t = ListType then
+    let arr : (obj array) = Array.zeroCreate l
+    let mutable cursor = 8
+    for entry = 0 to (l - 1) do
+      let (l, b) = decodeWithLength bstream.[cursor..]
+      cursor <- cursor + l
+      arr.[entry] <- b
+    let lst =  Array.toList arr
+    (cursor, (box lst))
+  else failwith "Not a list"
+and private mapKeyLength (bstream: byte array) =
+  // Find the null terminator for the string
+  let nullTerm = Seq.findIndex (fun x -> x = 0uy) bstream
+  // Round up to 8 byte boundary
+  let p = find8BytePadding nullTerm
+  (nullTerm, nullTerm + p)
+and private decodeMapLen (bstream: byte array) =
+  let (t, l) = getTypeAndLength bstream
+  if t = MapType then
+    let dict = Dictionary<string, obj>()
+    let mutable cursor  = 8
+    for entry = 0 to (l - 1) do
+      // First decode string key
+      let (slen, klen)  = mapKeyLength bstream.[cursor..]
+      let key = System.Text.Encoding.ASCII.GetString(bstream, cursor, slen)
+      cursor <- cursor + klen
+      let (l, value) = decodeWithLength bstream.[cursor..]
+      cursor <- cursor + l
+      dict.Add(key, value)
+    (cursor, (box dict))
+  else failwith "Not a map"
+
+let DecodeList (bstream: byte array) =
+  let (_, b) = (decodeListLen bstream)
+  unbox b
+
+let DecodeMap (bstream: byte array) =
+  let (_, b) = (decodeMapLen bstream)
+  unbox b
+
+(* Call for untyped decoding of byte array *)
+let Decode (bstream: byte array) : obj =
+  let (_, b) = (decodeWithLength bstream)
+  b
+
+(* Decode into an object *)
+let DecodeObject<'a when 'a : (new: unit -> 'a)> (bstream: byte array): 'a =
+  let t = new 'a()
+  let properties = t.GetType().GetProperties() |> 
+                   Array.filter (fun x -> x.CanWrite)
+  let m : Dictionary<string, obj> = DecodeMap bstream
+  Array.iter (fun (prop: PropertyInfo) ->
+      let k : string = prop.Name 
+      printfn "%s %A" k m
+      prop.SetValue(t, m.[k])
+  ) properties
+  t
