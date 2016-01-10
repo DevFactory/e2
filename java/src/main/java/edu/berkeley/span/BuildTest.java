5b1475d819da5cf0b9c030632e2b0809762fe659
Aurojit Panda <apanda@cs.berkeley.edu>
2016-01-09 21:19:02 -0800

    Protobuf and stuff that actually runs

--- java/src/main/java/edu/berkeley/span/BuildTest.java
+++ java/src/main/java/edu/berkeley/span/BuildTest.java
@@ -1,7 +1,12 @@
 package edu.berkeley.span;
+import edu.berkeley.span.Request.Command;
+import edu.berkeley.span.Request.Command.Builder;
 
 public class BuildTest {
 	public static void main(String[] args) {
+		Command.Builder builder = Command.newBuilder();
+		builder.setCommand(Command.Commands.ListRunning);
+		builder.build();
 		System.out.println("Hello from java land");
 	}
 }
